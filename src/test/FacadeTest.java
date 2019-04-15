3393b157aa14ec069f63c24dd3bf751002866fe3
Jason Guest <jaguest@asu.edu>
2019-04-15 12:17:51 -0700

    Began test class for facade

--- src/test/FacadeTest.java
+++ src/test/FacadeTest.java
@@ -4,11 +4,15 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import main.java.Facade.Facade;
+
 public class FacadeTest {
 
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testConstructor() {
+        Facade test = new Facade();
+        assertNotNull(test);
+        
     }
 
 }
