9c76142c7a40e518e1caab03249dd9777b5845f6
Jason Guest <jaguest@asu.edu>
2019-04-15 01:05:32 -0700

    Created a builder and reader class in builder package

---
+++ src/main/java/Builder/Builder.java
@@ -0,0 +1,8 @@
+package main.java.Builder;
+
+/**
+ * Receives build commands from the reader, runs the construction chain
+ */
+public class Builder {
+
+}
---
+++ src/main/java/Builder/Reader.java
@@ -0,0 +1,10 @@
+package main.java.Builder;
+
+/**
+ * The reader class specified by builder design pattern, receives the
+ * order from client to construct, passes the build request to the builder.
+ * The client will just be the main method passing info to reader.
+ */
+public class Reader {
+
+}
