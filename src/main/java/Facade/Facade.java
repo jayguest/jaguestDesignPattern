cfda6e99916f1f948eb861be45acf259bab5181c
Jason Guest <jaguest@asu.edu>
2019-04-15 02:09:05 -0700

    Created Facade package and class

---
+++ src/main/java/Facade/Facade.java
@@ -0,0 +1,12 @@
+package main.java.Facade;
+
+/**
+ * This class provides a simple interface to the more complex system.
+ * Essentially, we want it so that the client needs only provide small
+ * amounts of information to the facade interface to get the entirety
+ * of the construction, communication, and retrieval of builds handled
+ * behind the scenes where the client doesn't see.
+ */
+public class Facade {
+
+}
